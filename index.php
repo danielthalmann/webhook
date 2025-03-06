5bb27c9b63e4998031e2a017559e3e658113bfa3
Daniel Thalmann <daniel@thalmann.li>
2025-03-06 09:36:59 +0100

    add test

--- index.php
+++ index.php
@@ -6,12 +6,11 @@ $payload = json_decode($_POST['payload'] ?? '{}', true);
 json_encode($payload, JSON_PRETTY_PRINT);
 
 $fp = fopen(LOGFILE, 'w');
-fwrite($fp, json_encode($payload, JSON_PRETTY_PRINT));
+//fwrite($fp, json_encode($payload, JSON_PRETTY_PRINT));
+ExecuteCmd('pwd', $fp);
+ExecuteCmd('git remote -v', $fp);
 ExecuteCmd('git remote set-url origin https://github.com/danielthalmann/webhook.git', $fp);
 ExecuteCmd('git pull', $fp);
-echo "Returned with status $retval and output:\n";
-print_r($output);
-
 
 fclose($fp);
 chmod(LOGFILE, 0666);
@@ -20,7 +19,7 @@ function ExecuteCmd($cmd, $fp)
 {
     fwrite($fp, $cmd . "\n");
     exec($cmd, $output, $retval);
-    fwrite($fp, 'return : ' . $retval . "\n");
+    //fwrite($fp, 'return : ' . $retval . "\n");
     if ($output) {
         foreach ($output as $line) {
             fwrite($fp, $line . "\n");
