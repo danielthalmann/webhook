e85c964ceb1a6f17f0007aaf7373e81bf8a76d52
Daniel Thalmann <daniel@thalmann.li>
2025-03-06 08:56:29 +0100

    update index

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 
-$payload = json_decode($_POST['payload'], true);
-json_encode($payload, JSON_PRETTY_PRINT)
+$payload = json_decode($_POST['payload'] ?? '{}', true);
+json_encode($payload, JSON_PRETTY_PRINT);
 
 $fp = fopen('request.txt', 'w');
 fwrite($fp, json_encode($payload, JSON_PRETTY_PRINT));
 fclose($fp);
 chmod('request.txt', 0666);
- 
