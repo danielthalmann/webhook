05109d648f42ebb6e89c1a88f71ca13c883879f1
Daniel Thalmann <daniel@thalmann.li>
2025-03-06 08:51:22 +0100

    update index

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
 
-$req_dump = print_r($_REQUEST, true);
-$post_dump = print_r($_POST, true);
-$get_dump = print_r($_GET, true);
-//unlink('request.log');
-$fp = fopen('request.txt', 'a');
-fwrite($fp, $req_dump);
-fwrite($fp, $post_dump);
-fwrite($fp, $get_dump);
+$payload = json_decode($_POST['payload'], true);
+json_encode($payload, JSON_PRETTY_PRINT)
+$fp = fopen('request.txt', 'w');
+fwrite($fp, json_encode($payload, JSON_PRETTY_PRINT));
 fclose($fp);
 chmod('request.txt', 0666);
