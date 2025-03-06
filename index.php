6972867fa92efdce8c89df702251d5d37d92a2df
Daniel Thalmann <daniel@thalmann.li>
2025-03-06 08:47:42 +0100

    update index

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 $req_dump = print_r($_REQUEST, true);
 $post_dump = print_r($_POST, true);
 $get_dump = print_r($_GET, true);
-unlink('request.log');
-$fp = fopen('request.log', 'a');
+//unlink('request.log');
+$fp = fopen('request.txt', 'a');
 fwrite($fp, $req_dump);
 fwrite($fp, $post_dump);
 fwrite($fp, $get_dump);
 fclose($fp);
-chmod('request.log', 666);
+chmod('request.txt', 0666);
