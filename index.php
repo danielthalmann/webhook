337c49b0cac8c7b9b60a7185ffb82dd6fe027439
Daniel Thalmann <daniel@thalmann.li>
2025-03-06 09:25:28 +0100

    update index

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <?php
 
+const LOGFILE = 'request.log';
+
 $payload = json_decode($_POST['payload'] ?? '{}', true);
 json_encode($payload, JSON_PRETTY_PRINT);
 
-$fp = fopen('request.txt', 'w');
+$fp = fopen(LOGFILE, 'w');
 fwrite($fp, json_encode($payload, JSON_PRETTY_PRINT));
+ExecuteCmd('git remote set-url origin https://github.com/danielthalmann/webhook.git', $fp);
+ExecuteCmd('git pull', $fp);
+echo "Returned with status $retval and output:\n";
+print_r($output);
+
+
 fclose($fp);
-chmod('request.txt', 0666);
+chmod(LOGFILE, 0666);
+
+function ExecuteCmd($cmd, $fp)
+{
+    fwrite($fp, $cmd . "\n");
+    exec($cmd, $output, $retval);
+    fwrite($fp, 'return : ' . $retval . "\n");
+    if ($output) {
+        foreach ($output as $line) {
+            fwrite($fp, $line . "\n");
+        }
+    }
+}
